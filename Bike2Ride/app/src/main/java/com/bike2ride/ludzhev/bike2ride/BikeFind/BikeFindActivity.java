8d36118e81a36e427e05f9ac741d4e6b177d650e
ludzhev <ludzhev@gmail.com>
2017-10-09 09:08:17 +0300

    Add simple map.

---
+++ Bike2Ride/app/src/main/java/com/bike2ride/ludzhev/bike2ride/BikeFind/BikeFindActivity.java
@@ -0,0 +1,37 @@
+package com.bike2ride.ludzhev.bike2ride.BikeFind;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.bike2ride.ludzhev.bike2ride.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class BikeFindActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bike_find);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(42.650947, 23.379337);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Telerik Academy"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
